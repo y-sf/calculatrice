1d68a2a6c5d66a87180944846d41a2b4593b33cd
nchtmusfa <moussamirza46@gmail.com>
2024-10-04 16:39:58 +0200

    Ajout du script de Addition.py et Division.py

--- addition.py
+++ addition.py
@@ -1,1 +1,7 @@
 !/usr/bin/bash/python
+
+n1=4
+n2=8
+res=n1+n2
+
+print("le résultat de cette addition est",res)
--- division.py
+++ division.py
@@ -1,1 +1,8 @@
 !/usr/bin/bash/python
+
+n1=8
+n2=2
+res=n1/n2
+
+print("le résultat de cette division est",res)
+
